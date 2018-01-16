ceabeb92532aefd4f630b13aa6f28853aa6359b0
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-15 19:25:54 -0600

    1/15/18

---
+++ class-four-mini-three.js
@@ -0,0 +1,15 @@
+// 1. Write (declare) a function named multiplyThemUp 
+    // A. That accepts 3 arguments: all numbers
+    // B. Multiply all the numbers passed in together.
+    // C. Returns the product.
+
+    function multiplyThemUp(num1, num2, num3){
+        return product = num1 * num2 * num3;
+      }
+      // 2. Call the function multiplyThemUp, passing in your top 3 favorite numbers. Save the results of this function to a variable called myProduct. (Another way this instruction could be worded is: declare a variable named myProduct that gets its value by calling multiplyThemUp and passing in your top 3 favorite numbers.)
+      var myProduct = multiplyThemUp(102, 33, 12);
+      // Console log your variable myProduct.
+      console.log(myProduct)
+      
+      
+      
